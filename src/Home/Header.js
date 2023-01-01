68882131fd6d3dd24ab65a2eded68a21ba91cec3
Syed Saif Ali Shah <shahsaif99@gmai.com>
2023-01-01 18:24:27 +0500

    bugs fixed

--- src/Home/Header.js
+++ src/Home/Header.js
@@ -26,7 +26,7 @@ export default function Header(){
 <div>
 
 <button className="navbar-toggler me-auto tog-btn" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-<i className="fa fa-bars" style={{'color':'#fff', 'font-size':'28px'}}></i>
+<i className="fa fa-bars" style={{'color':'#fff', 'fontSize':'28px'}}></i>
 </button>
 </div>
 <a className="navbar-brand me-auto logo" href="#"><img src="/images/logo2.png"/></a>
@@ -58,43 +58,3 @@ export default function Header(){
 
 
 
-
-        // my code
-     
-//     <div classNameName="main">
-//         <nav class="navbar navbar-expand-lg ">
-//    <div class="container-fluid mt-4 ">
-//     <div>
-
-// <button class="navbar-toggler me-auto tog-btn" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-//   <span class="navbar-toggler-icon tog-btn"></span>
-// </button>
-//     </div>
-// <a class="navbar-brand me-auto" href="#">Website Logo</a>
-// <div class="collapse navbar-collapse" id="navbarNav">
-
-//   <ul class="navbar-nav ms-auto">
-//     <li class="nav-item">
-//       <a class="nav-link active"  href="#"> <i className="fa fa-instagram ic-logo" aria-hidden="true"></i></a>
-//     </li>
-//     <li class="nav-item">
-//       <a class="nav-link active" href="#"><i class="fa fa-facebook-square ic-logo" aria-hidden="true"></i></a>
-//     </li>
-    
-//   </ul>
-
-// </div>
-
-// <div className=" ms-auto">
-
-//     <i class="fa fa-shopping-bag logo-cart" aria-hidden="true"></i>
-    
-// </div>
-// </div>
-// </nav>
-
-//    <div className="main-text">Meet our new<br/> limited collection</div> 
-//    <div className="sub-text">Available now</div>
-//     </div>
-
-// my code
