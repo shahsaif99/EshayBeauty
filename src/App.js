fe72faf2ea1ce48e8e16c75a1ec0b65332efedd9
Syed Saif Ali Shah <shahsaif99@gmai.com>
2023-01-02 23:33:49 +0500

    code cleaned

--- src/App.js
+++ src/App.js
@@ -4,13 +4,7 @@ import Card from "./Home/Card"
 
 export default function App(){
     const [cards, setCards]=React.useState([])
-  const [cardData , setCardData]=React.useState({
-    productname: "",
-    productdis: "",
-
-  })
-
-
+    
   async function getcards(){
     const res= await fetch("https://jsonplaceholder.typicode.com/posts")
     setCards(await res.json());
@@ -28,17 +22,16 @@ export default function App(){
         {
             cards.map((data)=>{
                 return(
-                  <div className="card-list container-fluid">
-                    <div className="row">
-                        <div className="col-lg-4"> 
+                  
+                   
 
 
                     <Card 
                     name={data.title}
                     />
-                    </div>
-                    </div>
-                    </div>
+                    
+                    
+                    
 
                 )
             })
--- src/Home/Card.js
+++ src/Home/Card.js
@@ -5,36 +5,14 @@ import "bootstrap/dist/js/bootstrap.min.js";
 
 export default function Card(props) {
   
-//  console.log(cards.length)
-//  function catchingdata(){
-   
-//    return( 
-//   cards.map((obj)=>{
-//    console.log(cards.length)
-//    if(cards.length>0){
-//       console.log("helslo")
-//      var n=cards[1].title
-//      setCardData(prevdata =>({
-//        ...prevdata, productname: n
-//       }))
-//     }
-   
-   
-//    })
-// )
-// }
-// React.useEffect(()=>{
-//   catchingdata();
-
-// },[2])
-//  console.log(cardData)
 
 
   return (
     <div>
       
 
-          
+      <div className="row">
+                        <div className="col-lg-4"> 
 
           <div className="card mb-3">
             <img
@@ -60,79 +38,7 @@ export default function Card(props) {
             
         </div>
         
-              
+             </div></div> 
       
   );
 }
-
-//mycode
-//     <div>
-
-//     <div class="container-fluid">
-
-//     <div class="scrolling-wrapper row flex-row flex-nowrap mt-4 pb-4 pt-2" id="collection">
-
-// 			<div class="col-5">
-// 				<div class="card card-block card-1"></div>
-// 			</div>
-// 			<div class="col-5">
-// 				<div class="card card-block card-2"></div>
-// 			</div>
-// 			<div class="col-5">
-// 				<div class="card card-block card-3"></div>
-// 			</div>
-// 			<div class="col-5">
-// 				<div class="card card-block card-4"></div>
-// 			</div>
-// 			<div class="col-5">
-// 				<div class="card card-block card-5"></div>
-// 			</div>
-// 			<div class="col-5">
-// 				<div class="card card-block card-6"></div>
-// 			</div>
-// 			<div class="col-5">
-// 				<div class="card card-block card-7"></div>
-// 			</div>
-// 			<div class="col-5">
-// 				<div class="card card-block card-8"></div>
-// 			</div>
-// 			<div class="col-5">
-// 				<div class="card card-block card-9"></div>
-// 			</div>
-// 			<div class="col-5">
-// 				<div class="card card-block card-10"></div>
-// 			</div>
-//     </div>
-// </div>
-                          //  api call    
-//   const [card, setCard] = React.useState({
-    //     productname: "",
-    //     productprice: "",
-    //     productimg: "",
-    //   });
-    
-    //   const [allcard, setAllCard] = React.useState([{
-    //     userId:	1,
-    //   id:	1,
-    //  title:	"sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-    // body:	"quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-    //   }]);
-    
-    //   React.useEffect(() => {
-    //     fetch("https://jsonplaceholder.typicode.com/posts")
-    //       .then((res) => res.json())
-    //       .then((data) => setAllCard(data));
-    //   }, []);
-    //   console.log(allcard[2].id)
-      // if (allcard.length > 0) {
-      //   console.log(allcard[0].id);
-        // React.useEffect(()=>{
-    
-          
-        //   setCard((prevcard) => ({
-        //     ...prevcard,
-        //     productname: allcard[2].title,
-        //   }));
-        // },[]
-        //   )
-      
